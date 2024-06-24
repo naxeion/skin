f741faa6dba10046b16604c42bf746690e291f68
Naxeion <naxeion@proton.me>
2024-06-25 02:45:30 +0300

    Refactor CLI and args system to use cli.rs

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,21 @@
-use rskin::args;
+use clap::Parser;
+
 use rskin::as_root;
 use rskin::error;
-use rskin::handle_subcommand;
 use rskin::modules::command;
 use rskin::modules::db;
-use std::ffi::OsStr;
-use std::process;
 
-fn main() {
+mod cli;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 	if !as_root() {
 		color_print::cprintln!("<red,bold>Error:</> {}", error::ERROR_NOT_ROOT);
-		process::exit(0);
+		std::process::exit(0);
 	}
 
 	let _ = db::setup_database();
 	let _ = command::setup_config_dir(None);
 
-	let (mut cmd, matches) = args::parse_args();
-
-	if let Some((subcommand, sub_matches)) = matches.subcommand() {
-		let mut extras_arg: String = "".to_string();
-		if let Ok(status) = sub_matches.try_contains_id("extras") {
-			if status {
-				if let Some(mut extras) = sub_matches.get_raw("extras") {
-					let first_arg = extras.next().map_or_else(|| OsStr::new(""), |arg| arg);
-					let rest_of_arguments: Vec<String> = extras
-						.map(|arg| arg.to_string_lossy().to_string())
-						.collect();
-
-					let extras_as_string = first_arg.to_string_lossy().to_string();
-					let rest_as_string = rest_of_arguments.join(" ");
-
-					extras_arg = format!("{} {}", extras_as_string, rest_as_string);
-				}
-			}
-		}
-
-		handle_subcommand(subcommand, sub_matches, &extras_arg);
-	} else {
-		let _ = cmd.print_help();
-		println!();
-	}
+	let cli = cli::Cli::parse();
+	cli::execute_command(cli)
 }
