0d0e549540dfe496d25ca6f8336ab59fe021586f
Naxeion <naxeion@proton.me>
2024-06-25 03:02:32 +0300

    Update action function to use run_by_skinner and handle command execution

--- src/helper/run.rs
+++ src/helper/run.rs
@@ -1,43 +1,44 @@
 use chrono::Utc;
-use std::ffi::OsStr;
-use std::process::{Command, Stdio};
 
 use crate::helper::get_target_data;
 use crate::modules::db;
+use crate::skinners::run_by_skinner;
 use crate::{error, error::throw};
 
-pub fn action(target: &str, extras: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub fn action(target: &str, extras: &str) -> Result<bool, Box<dyn std::error::Error>> {
 	let db_conn = db::connect_to_main_db()?;
 
 	if let Ok(metadata_vec) = get_target_data(target) {
 		if let Some(target_metadata) = metadata_vec.first() {
-			let args: Vec<&str> = extras.split_whitespace().collect();
-			let args_to_pass: Vec<&OsStr> = args.iter().map(OsStr::new).collect();
 			let replaced_with = target_metadata.replaced_with.clone();
+			let skinner = target_metadata.skinner;
 
-			let mut cmd = Command::new(replaced_with.clone())
-				.args(&args_to_pass)
-				.stdin(Stdio::inherit())
-				.stdout(Stdio::inherit())
-				.stderr(Stdio::inherit())
-				.spawn()
-				.expect("Failed to execute process");
+			let success = run_by_skinner(target, skinner, Some(extras));
+			match success {
+				Ok(success) => {
+					if success {
+						let used_command = format!(
+							"{} {}",
+							replaced_with.unwrap_or(target.to_string()).clone(),
+							extras
+						);
+						let query = format!("UPDATE TARGETS SET LAST_USE_DATE='{date}', LAST_USE_CMD='{cmd}' WHERE TARGET='{target}';",
+								date = Utc::now(),
+								cmd = used_command,
+								target = target_metadata.target
+							);
 
-			let status = cmd.wait().expect("Failed to wait on command process");
-
-			if status.success() {
-				let used_command = format!("{} {}", replaced_with, extras);
-				let query = format!("UPDATE TARGETS SET LAST_USE_DATE='{date}', LAST_USE_CMD='{cmd}' WHERE REPLACED_WITH='{replaced_with}';",
-						date = Utc::now(),
-						cmd = used_command,
-						replaced_with = replaced_with
-					);
-
-				db::execute_sql(&db_conn, &query)?;
+						db::execute_sql(&db_conn, &query)?;
+					}
+				}
+				Err(_) => todo!(),
 			}
+
+			return success;
 		} else {
 			throw!(error::TARGET_METADATA_EMPTY)
 		}
 	}
-	Ok(())
+
+	Ok(false)
 }
