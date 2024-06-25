c7f94f20f3ea0abc6c32dfdf87c74c502f9daf6f
Naxeion <naxeion@proton.me>
2024-06-25 03:00:25 +0300

    Refactor disactivate.rs: Implement skinner existence check and handle builtin commands

--- src/helper/disactivate.rs
+++ src/helper/disactivate.rs
@@ -1,33 +1,43 @@
 use crate::modules::command;
 use crate::modules::db;
-use crate::skinners::r;
+use crate::skinners::is_skinner_exist;
+use crate::skinners::r#do;
+use crate::{error, error::throw};
 
-pub fn action(target: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub fn action(target: &str, skinner: &str) -> Result<bool, Box<dyn std::error::Error>> {
 	let paths = command::find_command_paths(target);
 	let db_conn = db::connect_to_main_db()?;
 
-	match paths {
-		Ok(ref paths) => {
-			if paths.is_empty() {
-				println!(
-					"The command `{}` does not exist or may have been deactivated previously",
-					target
-				);
-			}
-			for item in paths {
-				if let Some(skinner_metadata) = r::r#do(item) {
-					let query = format!("INSERT INTO TARGETS (TARGET, REPLACED_WITH, SKINNER, STATUS) VALUES ('{}', '{}', '{}', TRUE)", 
-                        skinner_metadata.target,
-                        skinner_metadata.replaced_with,
-                        skinner_metadata.skinner,
-                    );
-
-					db::execute_sql(&db_conn, &query)?;
-				}
-			}
-		}
-		Err(error) => panic!("Error searching for command paths: {:?}", error),
+	if command::is_builtin_command(target) {
+		throw!(error::CANNOT_DISACTIVATE_BUILTIN, exit);
 	}
 
-	Ok(())
+	if is_skinner_exist(skinner) {
+		match paths {
+			Ok(paths) => {
+				if paths.is_empty() {
+					throw!(error::TARGET_UNDEACTIVATABLE);
+					return Ok(false);
+				}
+				for item in paths {
+					if let Some(skinner_metadata) = r#do(&item, skinner) {
+						let query = format!("INSERT INTO TARGETS (TARGET, REPLACED_WITH, SKINNER, STATUS) VALUES ('{}', {}, '{}', TRUE)", 
+							skinner_metadata.target,
+							skinner_metadata.replaced_with.as_ref().map_or("Null".to_string(), |s| format!("'{}'", s)),
+							skinner_metadata.skinner,
+						);
+
+						db::execute_sql(&db_conn, &query)?;
+					}
+				}
+
+				return Ok(true);
+			}
+			Err(error) => throw!(format!("Error searching for command paths: {:?}", error), exit),
+		}
+	} else {
+		error::throw!(error::SKINNER_NOT_EXIST);
+	}
+
+	Ok(false)
 }
