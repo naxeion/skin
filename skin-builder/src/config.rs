f3644225f65661709e55e41ad9b6c32852b1740d
Naxeion <naxeion@proton.me>
2024-06-27 06:45:42 +0300

    Add SKIN_CONFIG_DIRECTORY constant in config.rs

--- skin-builder/src/config.rs
+++ skin-builder/src/config.rs
@@ -1,2 +1,3 @@
+pub const SKIN_CONFIG_DIRECTORY: &str = "/root/.skin/";
 pub const SKIN_SKINNERS_DIRECTORY: &str = "/root/.skin/skinners/";
 pub const SKIN_TEMP_DIRECTORY: &str = "/root/.skin/temp/";
