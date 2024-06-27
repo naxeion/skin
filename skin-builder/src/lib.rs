1f6784e5c60e49bc8bb244e1300c892a4624f3e9
Naxeion <naxeion@proton.me>
2024-06-27 06:48:03 +0300

    Add setup_config_dir and as_root functions in skin-builder/lib.rs

--- skin-builder/src/lib.rs
+++ skin-builder/src/lib.rs
@@ -1,5 +1,32 @@
+extern crate libc;
+
 pub mod config;
 pub mod error;
 pub mod installer;
 pub mod makefile;
 pub mod validator;
+
+use crate::config::{SKIN_CONFIG_DIRECTORY, SKIN_SKINNERS_DIRECTORY, SKIN_TEMP_DIRECTORY};
+use std::fs;
+
+pub fn setup_config_dir(custom_path: Option<&str>) -> Result<(), std::io::Error> {
+	for s_path in [
+		SKIN_CONFIG_DIRECTORY,
+		SKIN_SKINNERS_DIRECTORY,
+		SKIN_TEMP_DIRECTORY,
+	] {
+		let path = custom_path.unwrap_or(s_path);
+		let expanded_path = shellexpand::tilde(path).into_owned();
+
+		if fs::metadata(&expanded_path).is_err() {
+			fs::create_dir_all(&expanded_path)?;
+		}
+	}
+
+	Ok(())
+}
+
+pub fn as_root() -> bool {
+	let uid = unsafe { libc::getuid() };
+	uid == 0
+}
