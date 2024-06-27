6987ec04968e3ccc5e4c028ccca349d9ed209cd1
Naxeion <naxeion@proton.me>
2024-06-27 06:47:06 +0300

    Refactor skin-builder/main.rs: Use as_root and setup_config_dir functions

--- skin-builder/src/main.rs
+++ skin-builder/src/main.rs
@@ -1,18 +1,17 @@
 use clap::Parser;
 mod cli;
 
+use skin_builder::as_root;
+use skin_builder::error;
+use skin_builder::setup_config_dir;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	// println!("{:?}", validator::check_targets("./skinner/X/Makefile"));
+	if !as_root() {
+		color_print::cprintln!("<red,bold>Error:</> {}", error::ERROR_NOT_ROOT);
+		std::process::exit(0);
+	}
 
-	// if !as_root() {
-	// 	color_print::cprintln!("<red,bold>Error:</> {}", error::ERROR_NOT_ROOT);
-	// 	std::process::exit(0);
-	// }
-
-	// let _ = db::setup_database();
-	// let _ = command::setup_config_dir(None);
-
-	// check SKIN_SKINNERS_DIRECTORY if exist
+	let _ = setup_config_dir(None);
 
 	let cli = cli::Cli::parse();
 	cli::execute_command(cli)
