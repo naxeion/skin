295807c4603e3d72af2bff553011f887d3583598
Naxeion <naxeion@proton.me>
2024-07-08 04:42:44 +0300

    Update version to 1.1.0 and adjust success handling in execute_command function

--- skin-builder/src/cli.rs
+++ skin-builder/src/cli.rs
@@ -4,7 +4,7 @@ use tokio::runtime::Runtime;
 use skin_builder::installer;
 
 #[derive(Parser)]
-#[command(name = "skin-builder", version = "1.0.5")]
+#[command(name = "skin-builder", version = "1.1.0")]
 pub struct Cli {
 	#[command(subcommand)]
 	pub command: Commands,
@@ -19,9 +19,14 @@ pub fn execute_command(cli: Cli) -> Result<(), Box<dyn std::error::Error>> {
 	match cli.command {
 		Commands::Install { url } => {
 			let rt = Runtime::new()?;
-
 			rt.block_on(async {
-				if let Err(err) = installer::operate(&url).await {
+				if let Ok(success) = installer::operate(&url).await {
+					if success {
+						color_print::cprintln!(
+							"The skinner has been installed <green>successfully</green>."
+						);
+					}
+				} else if let Err(err) = installer::operate(&url).await {
 					eprintln!("Error installing file: {}", err);
 				}
 			});
